68f7f2387f956186599e1ff619be47f7a567cae9
HemantSharma08 <hs8112002@gmail.com>
2024-10-16 20:41:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+ import Joke from './components/joke'
+function App() {
+  
+  return (
+    <>
+    <div className="App">
+      <h1>Joke Generator Using React and joke APi</h1>
+      <Joke>
+      </Joke>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.jsx
@@ -0,0 +1,7 @@
+const Button=(props)=>{
+    return <button onClick={props.callApi}>
+        Click to generate a joke
+    </button>
+}
+
+export default Button;
